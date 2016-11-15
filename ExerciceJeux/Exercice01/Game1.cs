97b64e86102c2aec72ff354549c6680c941aecaf
Pat <charland.patrick@carrefour.cegepvicto.ca>
2016-11-14 21:57:44 -0500

    Update
    Added enemy and projectile movement.

--- ExerciceJeux/Exercice01/Game1.cs
+++ ExerciceJeux/Exercice01/Game1.cs
@@ -14,6 +14,8 @@ namespace Exercice01
         Rectangle fenetre;
         GameObject heros;
         GameObject ennemi;
+        GameObject projectile;
+        Texture2D backgroundTexture;
 
         public Game1()
         {
@@ -51,6 +53,7 @@ namespace Exercice01
             fenetre.Width = graphics.GraphicsDevice.DisplayMode.Width;
             fenetre.Height = graphics.GraphicsDevice.DisplayMode.Height;
 
+            this.backgroundTexture = this.Content.Load<Texture2D>("resized_daf_12685_super_mario_bros.jpg");
 
             heros = new GameObject();
             heros.estVivant = true;
@@ -63,6 +66,16 @@ namespace Exercice01
             ennemi.vitesse = 7;
             ennemi.sprite = Content.Load<Texture2D>("Bowserjr_MP9.png");
             ennemi.position = ennemi.sprite.Bounds;
+            ennemi.position.X = fenetre.Right - 300;
+            ennemi.position.Y = fenetre.Top + 20;
+
+            projectile = new GameObject();
+            projectile.estVivant = true;
+            projectile.vitesse = 10;
+            projectile.sprite = Content.Load<Texture2D>("Bullet_Bill.png");
+            projectile.position = projectile.sprite.Bounds;
+            projectile.position.X = ennemi.position.X;
+            projectile.position.Y = ennemi.position.Y;
 
             // TODO: use this.Content to load your game content here
         }
@@ -104,13 +117,26 @@ namespace Exercice01
             {
                 heros.position.Y += heros.vitesse;
             }
+
             //Ennemis move
 
+            ennemi.position.Y += ennemi.vitesse;
 
-                ennemi.position.X += ennemi.vitesse;
+            if (ennemi.position.Y <= fenetre.Top || ennemi.position.Y >= fenetre.Bottom - ennemi.sprite.Bounds.Height)
+            {
+                ennemi.vitesse *= -1;
+            }
 
-                ennemi.position.X -= ennemi.vitesse;
-
+            //Projectile move
+            
+            projectile.position.X -= projectile.vitesse;
+            if(projectile.position.X < fenetre.Left - 500)
+            {
+                projectile.estVivant = false;
+                projectile.position.X = ennemi.position.X;
+                projectile.position.Y = ennemi.position.Y;
+                projectile.estVivant = true;
+            }
 
             UpdateHeros();
 
@@ -146,8 +172,14 @@ namespace Exercice01
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+
+            this.spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, backgroundTexture.Width, backgroundTexture.Height), Color.White);
+
+     
             spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            spriteBatch.Draw(projectile.sprite, projectile.position, Color.White);
             spriteBatch.Draw(ennemi.sprite, ennemi.position, Color.White);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
